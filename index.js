5fa0b140213a0b99c819d47d1ad8322ba11ff98b
daftarTeraOnline <pendataanuttp.ska@gmail.com>
2023-03-30 14:45:13 +0700

    membuat UI aplikasi 8

--- index.js
+++ index.js
@@ -13,7 +13,38 @@ fetch(url)
 });
 
 let nextBtn = document.getElementById("next");
-
 nextBtn.addEventListener('click',() => {
     location.href="#sub2";
 });
+
+let addBtn = document.querySelector(".addDiv");
+addBtn.addEventListener('click',() => {
+    setTimeout(() => {
+        document.querySelector(".uttpDiv").style.display = "flex";
+        document.querySelector(".uttpDiv").style.flexDirection = "row";
+        document.querySelector(".uttpDiv").style.flexWrap = "wrap";
+        document.querySelector(".uttpDiv").style.justifyContent = "flex-start";
+        document.querySelector(".uttpDiv").hidden = false;  
+    }, 300);
+});
+
+let uttpArrPic = [
+    ['TBI','img/TBI.jpg'],
+    ['TE','img/TE.webp'],
+    ['TE','img/TE_analytic.webp'],
+    ['TE','img/TE_hanging.webp'],
+    ['TE','img/TE2.webp'],
+    ['TE','img/TE3.jfif'],
+    ['TM','img/TM.jfif'],
+    ['TP','img/TP.jfif'],
+    ['TP','img/TP2.webp'],
+    ['TP','img/TP3.webp'],
+    ['TS','img/TS.jpg'],
+    ['DL','img/DL.jpg'],
+    ['N','img/N.jpg'],
+    ['MK','img/MK.jpg']
+];
+
+for (k in uttpArrPic) {
+    console.log(uttpArrPic[k]);
+}
