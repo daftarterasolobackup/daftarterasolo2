456fed4ef100415caad409f3b4f03faba9657fe8
belajarwebdevelopment <belajar.webdevelopment@gmail.com>
2023-06-10 23:25:31 +0700

    memebuat form menggunakan class OOP 7

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//const { resolve } = require("path");
-
 async function getKelurahan() {
 	url = "https://script.google.com/macros/s/AKfycbxNbS3tE2nm7KCbhIFw71dkBpwZn0MJWPbM2b7mhot7a3Ir0WxB3wCFCAnYOE38Dvo/exec";
 
@@ -17,16 +15,12 @@ async function getKelurahan() {
 
 async function listOfUttpMasy() {
 	let url = "https://script.google.com/macros/s/AKfycby7EeCgoLpKAKNy9En83dAIg9cgovMEPrbwl9bNYI9M0-Br50gG53fBOpwi3p_3PQQ/exec";		
-	return await fetch(url).then(datas => datas.json()).then(datas => datas.uttp);		
+	return await fetch(url).then(datas => datas.json()).then(datas => datas.uttp.filter(e => {return e[0] != "PUBBM" && e[0] != "TJE"}));		
 }
 
 async function listOfUttpPabrik() {
 	let url = "https://script.google.com/macros/s/AKfycbzH27ZTSKFF2Q2xXIRQEs7sd6uNzTfZpp-9BvO9a5JDDrhnf_YHzqc_TYoMtdDNOVI/exec";
-	let mydata = [];
-	await fetch(url).then(datas => datas.json()).then(datas => {
-		mydata = datas.uttp.filter(e => {return e[0] != "N" && e[0] != "TP" && e[0] != "Meter Kayu" && e[0] != "PUBBM" && e[0] != "TJE"});
-	});		
-	return mydata;
+	return await fetch(url).then(datas => datas.json()).then(datas => datas.uttp.filter(e => {return e[0] != "N" && e[0] != "TP" && e[0] != "Meter Kayu" && e[0] != "PUBBM" && e[0] != "TJE"}));		
 }
 
 class createFormMasy {
